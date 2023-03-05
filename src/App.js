8fa449d7bb00ee7e38dfc42d972c547513e6f432
Mulero Alamou <Mulero.alamou@gmail.com>
2023-03-05 00:12:52 -0800

    Added react router and externalized home comp

--- src/App.js
+++ src/App.js
@@ -1,74 +1,26 @@
+import {BrowserRouter as Router, Route, Routes as Switch} from 'react-router-dom';
 import './App.css';
 import {useState} from 'react';
 import NavBar from './components/navbar';
-import image from './images/watermelon-leaf-anthracnose.jpg';
-import leafBlotch from './images/Necrotic_leaf_blotch1-WWT-A.jpg';
-import leafRot from './images/PLPATH-FRU-07-Black-Rot-Frogeye-Apple-figure-2.png';
-import TheosAPI from './components/theosapi'; 
+import Home from './components/Home';
+import About from './components/About';
+import Trivia from './components/Trivia';
+
 
 function App() {
-  const [img,setImg] = useState();
-  const url = "";
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    console.log(img);
-    // const response = await fetch(url, {
-    //   method:"POST",
-    //   body: 
-    // })
-  }
   return (
-    <div className="App">
-      <NavBar/>
-      <div className="body">
-        <h1>Welcome to our Plant Disease Detector</h1>
-        {/* insert gif here */}
-        <div className="one">
-          <section className="introSection">
-            <div className="introText">
-              <h2>What are plant diseases?</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores non illo expedita obcaecati, officia deserunt molestias laborum assumenda voluptatum dolore? Numquam, voluptatibus? Saepe adipisci accusamus iste eum eligendi voluptatem delectus!
-              </p>
-            </div>
-
-            <img src={image}/>
-          </section>        </div>          
-        <div className="colorBand"></div>
-        <div className="colorBand2"></div>
-        <div className="two">
-          <section className="functionality">
-            <div className='functionText'>
-              <h2>What Does our Program Do?</h2>
-                <p>
-                </p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero aliquid blanditiis, culpa praesentium odio animi sint commodi officiis, accusamus eligendi hic inventore rem ipsa similique aspernatur dicta iure tempora. Quibusdam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure distinctio molestiae unde, accusantium id hic nulla repellendus earum aspernatur quo vel velit, illo tempora doloremque, totam laborum nesciunt nemo. Nisi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae maxime blanditiis ipsum magni saepe sint facilis quia labore ipsam nostrum, sunt deserunt accusantium, laudantium, autem laboriosam rem totam! Corrupti, modi!
-              </div>
-              <div className="parent">
-                <div className="child1">
-                  <img src={leafRot} alt="" />
-                  <p className="child1txt">
-                    Apple Rot Leaf
-                  </p>
-                </div>
-                <div className="child2">
-                  <img src={leafBlotch} alt="" />  
-                  <p className="childwtxt">
-                    Leaf Blotch
-                  </p>
-                </div>
-              </div>
-
-              <h3>Upload your image here and find out what disease your plant has!</h3>
-            <TheosAPI/> 
-          </section>
-          
-        </div>
-        
+    <Router>
+      <div className="App">
+        <NavBar/>
+        <Switch>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/trivia' element={<Trivia/>}/>
+        </Switch>
+        <Home/>
       </div>
-    </div>
+    </Router>
   );
 }
 
