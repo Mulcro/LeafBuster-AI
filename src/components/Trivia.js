6d91f12dabcc1149713257cb3f028da8b6ee05b4
Mulero Alamou <Mulero.alamou@gmail.com>
2023-03-05 00:08:01 -0800

    initial commit

---
+++ src/components/Trivia.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Trivia() {
+  return (
+    <div>
+        <section className="triv1">
+            <div className='tc1'>
+                <h1>Leaf</h1>
+            </div>
+            <div className='tc2'>
+                <img src="" alt="" />
+            </div>
+            <div className='tc3'>
+                <h1>Triva!</h1>
+            </div>
+        </section>
+    </div>
+  )
+}
