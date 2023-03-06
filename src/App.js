69e12961d51f2e262bb65470fb05def2e881aa04
Mulero Alamou <Mulero.alamou@gmail.com>
2023-03-06 09:41:33 -0800

    Added route for unexpected paths

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
           <Route path='/home' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
           <Route path='/trivia' element={<Trivia/>}/>
+          <Route path='*' element={<Home/>}/>
         </Switch>
       </div>
     </Router>
