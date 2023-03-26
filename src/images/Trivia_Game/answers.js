9f89371e64485b172dff98bc859288c0e399ba9a
Mulero Alamou <Mulero.alamou@gmail.com>
2023-03-25 18:47:45 -0700

    Added images and answer object for trivia game

---
+++ src/images/Trivia_Game/answers.js
@@ -0,0 +1,14 @@
+// This file contains the correct answers for the trivia questions
+
+//0 = Leaf Rot
+//1 = Leaf Blotch
+
+const Answers = {
+    1: 0,
+    2: 0,
+    3: 1,
+    4: 0,
+    5: 1,
+}
+
+module.exports = Answers;
