08fe79a3891832aa3b37aca9d1ea1db297d51717
Mulero Alamou <Mulero.alamou@gmail.com>
2023-03-25 18:38:17 -0700

    Removed difficulty setting and cleaned up code

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,37 +1,26 @@
 import React, { useState } from 'react';
 import img1 from '../images/PLPATH-FRU-07-Black-Rot-Frogeye-Apple-figure-2.png';
 import trviaGif from '../images/quiz-night.gif';
-import Game from '../components/Game';
+import Popup from './GamePopup';
 
 export default function Trivia() {
-    const [param,setParam] = useState();
-    const [renderGame, setRenderGame] = useState(false);
-    const difficulties = {
-        "novice":0,
-        "expert":1
+    const [gameTrigger, setGameTrigger] = useState(false);
+
+
+    const handleGameStart = () => {
+        console.log("working");
+        setGameTrigger(true);
+        console.log(gameTrigger);
     }
 
-    const handleGameStart = (e,param) => {
-        
-    }
   return (
-    <>
-    {!renderGame && 
     <div className="trvDiv">
         
         <h1>Leaf Disease Trivia!</h1>
         <section className="triv1">
-            {/* <div className='tc1'>
-                <h3>Apple leaf blotch</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat beatae, illum impedit delectus nulla saepe cumque iste velit optio non odio recusandae sequi ex aut numquam eum magni distinctio quisquam!</p>
-            </div> */}
             <div className='tc2'>
                 <img src={img1} alt="" />
             </div>
-            {/* <div className='tc3'>
-                <h3>Apple leaf rot</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus quae soluta obcaecati unde illum mollitia tempore maiores maxime atque, veniam similique dolorem facilis provident distinctio ab dolores aperiam animi.</p> 
-        </div>*/}
         </section>
         <section className="trvDiv2">
             <div>
@@ -52,24 +41,11 @@ export default function Trivia() {
             <div className="trvLaunch">
                 <img src={trviaGif} alt="" />
                 <div>
-                    <select required onChange={e => setParam(e.target.value)} name="difficulty" id="">
-                        <option value="">-Select difficulty-</option>
-                        {Object.keys(difficulties).map(key => {
-                            return(
-                                <option value={difficulties[key]}>{key}</option>
-                            )
-                        })}
-                    </select>
-                    <button onClick={e => handleGameStart(e,param)}>Start Game</button>
+                   <button onClick={e => handleGameStart(e)}>Start Game</button> 
                 </div>
-                
+                <Popup trigger={gameTrigger} setTrigger={setGameTrigger}/>
             </div>
         </section>
     </div>
-    }
-    {renderGame &&
-        <Game param={param}/>
-    }
-    </>
   )
 }
