e29fb7179d439522afd3961c8b2c9d389eedc54b
Mulero Alamou <Mulero.alamou@gmail.com>
2023-03-25 18:35:07 -0700

    cleaned up code that wasn't needed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import {BrowserRouter as Router, Route, Routes as Switch} from 'react-router-dom';
 import './App.css';
-import {useState} from 'react';
 import NavBar from './components/navbar';
 import Home from './components/Home';
 import About from './components/About';
