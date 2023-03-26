0bc7e71da241cfee1b98dbf141b7e21345542d59
Mulero Alamou <Mulero.alamou@gmail.com>
2023-03-25 18:40:32 -0700

    refactored code

--- src/components/About.js
+++ src/components/About.js
@@ -11,8 +11,10 @@ export default function About() {
         <p>Together, our team brings a wealth of diverse backgrounds and experience to the table, and we are excited to present our AI project to contribute to Farm Food Future Innovation.</p>
 
         <h3>Check us out!</h3>
-            <p>Github: https://github.com/Mulcro/HackMerced-Project</p>
-        <h5>Made to win :)</h5>
+            <ul className='aboutList'>
+              <li><a href="https://github.com/Mulcro/HackMerced-Project">Github</a></li>
+              <li><a href="https://devpost.com/software/leaf-buster-ai">Devpost</a></li>
+            </ul>  
       </div>
     </div>
   )
